1b7c22bd34e8c65fa655008bb6caf96028bdb02a
Nedim Džibrić <47319999+Nedimm23@users.noreply.github.com>
2019-11-21 08:37:39 +0100

    new things added

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,23 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import { Platform } from "react-native";
+import Colors from "../constants/Colors";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primary : ""
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
+    }
+  }
+);
+
+export default createAppContainer(ProductsNavigator);
